c194d3ef810eec37f4dd0da0eca800217b7f6542
Mirae Park <pmr930105@gmail.com>
2017-04-04 21:56:59 +0900

    Basement

---
+++ app/src/main/java/com/example/samsung/considerationway/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.samsung.considerationway;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class IntroActivity extends AppCompatActivity {
+
+    //배경색 추가
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
