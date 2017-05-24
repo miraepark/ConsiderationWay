39803e76ac9986857719372a72c5573e8542d693
Mirae Park <pmr930105@gmail.com>
2017-05-24 11:14:40 +0900

    [adding]lineicon,lostnfound

--- app/src/main/java/com/example/samsung/considerationway/Call.java
+++ app/src/main/java/com/example/samsung/considerationway/Call.java
@@ -52,25 +52,49 @@ public class Call extends AppCompatActivity {
 
 
         mMyAdapter.addItem(
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon)
-                ," 광운대"
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l1)
+                ," 광운대역"
                 , "02-917-7445");
         mMyAdapter.addItem(
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon)
-                ," 구로 "
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l1)
+                ," 구로역 "
                 , "02-869-0089");
         mMyAdapter.addItem(
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon)
-                ," 병점"
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l1)
+                ," 병점역"
                 , "031-234-7788");
         mMyAdapter.addItem(
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon)
-                , " 시청"
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l12)
+                , " 시청역"
                 , "02-6110-1122");
         mMyAdapter.addItem(
-                ContextCompat.getDrawable(getApplicationContext(), R.drawable.icon)
-                , " 왕십리"
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l258)
+                , " 왕십리역"
                 , "02-6311-6765");
+        mMyAdapter.addItem(
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l3)
+                , " 대곡역"
+                , "031-965-8516");
+        mMyAdapter.addItem(
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l34)
+                , " 충무로역"
+                , "02-6110-3344");
+        mMyAdapter.addItem(
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l4)
+                , " 안산역"
+                , "031-491-7788");
+        mMyAdapter.addItem(
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l67)
+                , " 태릉입구역"
+                , "02-6311-6766");
+        mMyAdapter.addItem(
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l9)
+                , " 동작역"
+                , "02-2656-0009");
+        mMyAdapter.addItem(
+                ContextCompat.getDrawable(getApplicationContext(), R.drawable.l9air)
+                , " 김포공항역"
+                , "032-745-7777");
 
         mListView.setAdapter(mMyAdapter);
 
